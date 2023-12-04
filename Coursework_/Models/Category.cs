018436208a444c3955cb3d30553d44087d410665
Pugach-Nazar <qwertyasd0933@gmail.com>
2023-12-04 23:05:40 +0200

    models done

---
+++ Coursework_/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace Coursework_.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Підкатегорії
+        public int? ParentCategoryId { get; set; }
+        public Category ParentCategory { get; set; }
+        // Зв'язок один до багатьох з товарами
+        public List<Product> Products { get; set; }
+    }
+}

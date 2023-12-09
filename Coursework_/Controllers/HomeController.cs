f5e05a0c7198c9cc075ddb029a4e99c1553d250d
mulykoV <144117863+mulykoV@users.noreply.github.com>
2023-12-09 20:57:10 +0200

    Update HomeController.cs

--- Coursework_/Controllers/HomeController.cs
+++ Coursework_/Controllers/HomeController.cs
@@ -1,30 +1,52 @@
-﻿using Coursework_.Data;
-using Coursework_.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Linq;
+using Coursework_.Data;
+using Coursework_.Models;
+using Coursework_.ViewModels;
 
-namespace Coursework_.Controllers
+namespace YourNamespace.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        public HomeController(ApplicationDbContext dbContext)
         {
-            _logger = logger;
             _dbContext = dbContext;
         }
 
         public IActionResult Index()
         {
+            var products = _dbContext.Products
+                .Include(p => p.Category)
+                .Include(p => p.Manufacturer)
+                .Select(p => new ProductViewModel(p, p.Category, p.Manufacturer))
+                .ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Options()
+        {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Search(string searchString)
+        {
+            var products = _dbContext.Products
+                .Include(p => p.Category)
+                .Where(p => p.Name.Contains(searchString) || p.Model.Contains(searchString))
+                .Select(p => new ProductViewModel(p, p.Category, p.Manufacturer))
+                .ToList();
+
+            return View("Index", products);
         }
 
         public IActionResult Privacy()
         {
-            var categories = _dbContext.Categories.Include(c=>c.ChildCategories).FirstOrDefault(c=>c.Id == 1);
             return View();
         }
 

aec7bdd75d9fc5d1af0ec090a8f84b6bdd0e5e58
mulykoV <144117863+mulykoV@users.noreply.github.com>
2023-12-10 21:20:06 +0200

    Create ShopCart.cs

---
+++ Coursework_/Models/ShopCart.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using Coursework_.Data;
+
+namespace Coursework_.Models
+{
+    public class ShopCart
+    {
+        public readonly ApplicationDbContext _dbContext;
+        public ShopCart(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        
+        }
+        
+        [Key]
+        public string ShopCartId { get; set; }
+
+        public List<ShopItem> listShopItems { get; set; }
+
+        public static ShopCart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = services.GetService<ApplicationDbContext>();
+            string shopCartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CartId", shopCartId);
+
+            return new ShopCart(context) { ShopCartId = shopCartId };
+        }
+
+        public void AddToCart(Product product)
+        {
+            this._dbContext.ShopItems.Add(new ShopItem
+            {
+                ShopCartId = ShopCartId,
+                product = product,
+                Price = product.Price,
+            });
+
+            _dbContext.SaveChanges();
+        }
+
+        public List<ShopItem> GetShopItems()
+        {
+            return _dbContext.ShopItems.Where(c => c.ShopCartId == ShopCartId)
+                .Include(s => s.product)
+                .ToList();
+        }
+    
+    }
+
+}

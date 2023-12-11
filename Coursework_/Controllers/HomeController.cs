1da095b7fe53ccb8e3f7938de23fa7b804faafe1
Vova Mulyko <Vovamulyko@outlook.com>
2023-12-11 23:27:45 +0200

    Commit vova

--- Coursework_/Controllers/HomeController.cs
+++ Coursework_/Controllers/HomeController.cs
@@ -12,11 +12,13 @@ namespace Coursework_.Controllers
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly ShopCart _cart;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public HomeController(ApplicationDbContext dbContext, ShopCart cart)
+        public HomeController(ApplicationDbContext dbContext, ShopCart cart, IHttpContextAccessor httpContextAccessor)
         {
             _dbContext = dbContext;
             _cart = cart;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
@@ -55,68 +57,111 @@ namespace Coursework_.Controllers
         public RedirectToActionResult AddToCart(int id)
         {
             var item = _dbContext.Products.FirstOrDefault(i => i.Id == id);
-            if (item != null && item.Amount > 0) // Перевірка наявності товару
-            {
-                _cart.AddToCart(item); // Додаємо товар до кошика
-
-                // Віднімаємо одиницю від кількості товару
-                item.Amount--;
-
-                // Оновлюємо зміни в базі даних (або вашому сховищі)
-                _dbContext.SaveChanges(); // Зберігаємо зміни у базі даних
-            }
+            _cart.AddToCart(item);
             return RedirectToAction("Index");
         }
 
         public IActionResult Options()
         {
             return View();
-            //commit
         }
 
-        [HttpGet]
-        public IActionResult Search(string searchString)
-        {
-            var products = _dbContext.Products
-                .Include(p => p.Category)
-                .Where(p => p.Name.Contains(searchString) || p.Name.Contains(searchString))
-                .ToList();
 
-            var productViewModels = products.Select(p => new ProductViewModel(p)).ToList();
-
-            return View("Index", productViewModels);
-        }
 
         [HttpGet]
         public IActionResult Buy(int? id)
         {
-            if (!id.HasValue)
+            var itemsInCart = _cart.GetShopItems();
+
+            if (id.HasValue)
             {
-                return NotFound();
+                var item = _dbContext.Products.FirstOrDefault(i => i.Id == id.Value);
+
+                if (item != null && item.Amount > 0) // Перевірка наявності товару
+                {
+                    // Додаємо товар до кошика
+
+                    // Віднімаємо одиницю від кількості товару
+                    item.Amount--;
+
+                    // Оновлюємо зміни в базі даних (або вашому сховищі)
+                    _dbContext.SaveChanges(); // Зберігаємо зміни у базі даних
+                }
+                else
+                {
+                    TempData["EmptyCart"] = "Корзина порожня";
+                    return RedirectToAction("Index");
+                }
             }
 
-            var item = _dbContext.Products.FirstOrDefault(i => i.Id == id);
-            if (item == null)
+            if (!itemsInCart.Any())
             {
-                return NotFound();
+                TempData["EmptyCart"] = "Корзина порожня";
+                return RedirectToAction("Index");
             }
 
-            return View(item);
+            var purchases = itemsInCart.Select(item => new PurchaseViewModel
+            {
+                UserName = "", // Ініціалізуйте інші необхідні властивості покупки
+                DeviceId = id ?? 0 // Використовуємо DeviceId отриманий з параметра методу або за замовчуванням 0
+            }).ToList();
+
+            var purchaseListViewModel = new PurchaseListViewModel
+            {
+                Purchases = purchases
+            };
+
+            return View(purchaseListViewModel);
         }
 
         [HttpPost]
-        public IActionResult Buy(Purchase purchase)
+        public IActionResult Buy(PurchaseListViewModel purchaseListViewModel)
         {
-            purchase.DateTime = DateTime.Now;
-            _dbContext.Purchases.Add(purchase);
-            _dbContext.SaveChanges();
+            if (!_cart.HasItemsInCart(_httpContextAccessor.HttpContext.Session))
+            {
+                TempData["EmptyCart"] = "Корзина порожня";
+                return RedirectToAction("Index");
+            }
 
+            if (!ModelState.IsValid || purchaseListViewModel.Purchases == null || !purchaseListViewModel.Purchases.Any())
+            {
+                // Повернути представлення з PurchaseListViewModel у випадку недійсних даних або порожнього списку покупок
+                return View(purchaseListViewModel);
+            }
+
+            // Логіка для збереження покупок в базі даних
+            foreach (var purchaseViewModel in purchaseListViewModel.Purchases)
+            {
+                var purchase = new Purchase
+                {
+                    UserName = purchaseViewModel.UserName,
+                    DeviceId = purchaseViewModel.DeviceId,
+                    DateTime = DateTime.Now,
+                };
+
+                _dbContext.Purchases.Add(purchase);
+            }
+
+            // Збереження всіх покупок в базі даних
+            _dbContext.SaveChanges();
+            _httpContextAccessor.HttpContext.Session.Clear();
             _cart.ClearCart();
 
             TempData["PurchaseSuccess"] = "Покупку оформлено успішно!";
 
             return RedirectToAction("Index");
         }
+
+        public IActionResult Clear()
+        {
+            _cart.ClearCart(); // Очищення кошика
+
+            _httpContextAccessor.HttpContext.Session.Clear();
+
+            return RedirectToAction("Index");
+        }
+
+
 
         public IActionResult Privacy()
         {
